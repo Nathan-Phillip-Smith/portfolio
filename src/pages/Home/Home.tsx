9418702fac37cc2045f319ad6bdfddeb0881e5d0
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-08-29 09:12:26 -0600

    added projects

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,32 @@
+import { ChangeEvent } from 'react'
 import './Home.css'
 import 'animate.css'
+import PDF from './RESUME.pdf'
 
 const Home = () => {
+  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value !== '0') {
+      if (e.target.value === '1') {
+        window.open('https://capstone-86p9.onrender.com', '_blank')
+      } else if (e.target.value === '2') {
+        window.open(
+          'https://nathan-phillip-smith.github.io/buzzfeed-clone/',
+          '_blank'
+        )
+      } else if (e.target.value === '3') {
+        window.open(
+          'https://nathan-phillip-smith.github.io/imageGallery',
+          '_blank'
+        )
+      } else if (e.target.value === '4') {
+        window.open(
+          'https://nathan-phillip-smith.github.io/monster-egg-game/',
+          '_blank'
+        )
+      }
+    }
+  }
+
   return (
     <>
       <section className="business-card">
@@ -26,22 +51,36 @@ const Home = () => {
           </div>
           <div className="info-items">
             <p className="info-headline">Projects / Certificates</p>
-            <a href="https://capstone-86p9.onrender.com">•Full Stack App</a>
-            <a href="#">•Other Projects</a>
+            <div>
+              •
+              <select onChange={onSelectChange}>
+                <option value="0">Select a project</option>
+                <option value="1">
+                  Student Registration App (Take time to spin up)
+                </option>
+                <option value="2">BuzzFeed Clone</option>
+                <option value="3">Photo Gallery</option>
+                <option value="4">Monster Egg Game</option>
+              </select>
+            </div>
             <a href="https://www.freecodecamp.org/Nathan_Smith">
-              •freeCodeCamp Certificates
+              • freeCodeCamp Certificates
             </a>
             <a href="https://www.credly.com/badges/27323c0d-c952-402f-96b0-1d78f8f33ad1/linked_in?t=rxqsyk">
-              •Other Certificates
+              • Other Certificates
             </a>
           </div>
 
           <div className="info-items">
             <p className="info-headline">Socials / Source Code</p>
-            <a href="https://github.com/Nathan-Phillip-Smith">•GitHub</a>
+            <a href="https://github.com/Nathan-Phillip-Smith">• GitHub</a>
             <a href="https://www.linkedin.com/in/nathan-phillip-smith-3b5aa457/">
-              •LinkedIn
+              • LinkedIn
             </a>
+          </div>
+          <div className="info-items">
+            <p className="info-headline">Resume</p>
+            <a href={PDF}>• View</a>
           </div>
         </div>
       </section>
